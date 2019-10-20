072d7b7e455087fe88359d1f6c426bab06817e9e
unknown <ohndaniel@gmail.com>
2019-10-20 14:42:14 -0700

    Add project.

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+import '../styles/App.css'
+
+function App() {
+  const [cat, setCat] = useState()
+  const [error, setError] = useState()
+
+  const getCat = () => {
+    setCat()
+    setError()
+
+    fetch('http://aws.random.cat/meow')
+      .then(res => res.json())
+      .then(res => setCat(res))
+      .catch(e => setError(e.message))
+  }
+
+  return (
+    <div className="content">
+      <h1>Cat Project</h1>
+      <button onClick={getCat}>Get Cat</button>
+      {error && (
+        <div className="error">
+          <p>{error}</p>
+        </div>
+      )}
+      <div className="cat">
+        {cat && <img src={cat.file} alt="cat, a kitty cat" />}
+      </div>
+    </div>
+  )
+}
+
+export default App
